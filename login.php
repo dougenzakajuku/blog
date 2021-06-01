ab7e1fdfd356fa716e3fc7805730fe2a75a9f65f
tanaka katsuya <gu0520kt@gmail.com>
2021-06-01 10:51:55 +0900

    fix warnig errer in login.php

--- login.php
+++ login.php
@@ -14,17 +14,16 @@ $sql = "SELECT * FROM users WHERE email = :email";
 $stmt = $pdo->prepare($sql);
 $stmt->bindValue(':email', $email);
 $stmt->execute();
-$member = $stmt->fetch();
-//指定したハッシュがパスワードにマッチしているかチェック
-if (password_verify($_POST['password'], $member['password'])) {
+$member = $stmt->fetch(PDO::FETCH_ASSOC);
+if (!$member) {
+    $msg = 'メールアドレスもしくはパスワードが間違っています。';
+    $link = '<a href="login_form.php">戻る</a>';
+} elseif (password_verify($_POST['password'], $member['password'])) {
     //DBのユーザー情報をセッションに保存
     $_SESSION['id'] = $member['id'];
     $_SESSION['user_name'] = $member['user_name'];
     $msg = 'ログインしました。';
     $link = '<a href="index.php">ホーム</a>';
-} else {
-    $msg = 'メールアドレスもしくはパスワードが間違っています。';
-    $link = '<a href="login_form.php">戻る</a>';
 }
 ?>
 
