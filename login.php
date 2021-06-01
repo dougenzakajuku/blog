017e22131028f094e43207be1351b33c0308404b
tanaka katsuya <gu0520kt@gmail.com>
2021-06-01 09:31:49 +0900

    make login function

---
+++ login.php
@@ -0,0 +1,32 @@
+<?php
+session_start();
+$email = $_POST['email'];
+$dsn = "mysql:host=localhost; dbname=blog; charset=utf8mb4";
+$db_account_name = "blog";
+$db_account_password = "blog";
+try {
+    $pdo = new PDO($dsn, $db_account_name, $db_account_password);
+} catch (PDOException $e) {
+    $msg = $e->getMessage();
+}
+
+$sql = "SELECT * FROM users WHERE email = :email";
+$stmt = $dbh->prepare($sql);
+$stmt->bindValue(':email', $email);
+$stmt->execute();
+$member = $stmt->fetch();
+//指定したハッシュがパスワードにマッチしているかチェック
+if (password_verify($_POST['password'], $member['password'])) {
+    //DBのユーザー情報をセッションに保存
+    $_SESSION['id'] = $member['id'];
+    $_SESSION['user_name'] = $member['user_name'];
+    $msg = 'ログインしました。';
+    $link = '<a href="index.php">ホーム</a>';
+} else {
+    $msg = 'メールアドレスもしくはパスワードが間違っています。';
+    $link = '<a href="login.php">戻る</a>';
+}
+?>
+
+<h1><?php echo $msg; ?></h1>
+<?php echo $link; ?>
