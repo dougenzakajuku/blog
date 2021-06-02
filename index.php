96d57142f81fd751ec714032f098e9bf2fbc6940
tanaka katsuya <gu0520kt@gmail.com>
2021-06-02 09:11:16 +0900

    blogの一覧表示機能の作成

--- index.php
+++ index.php
@@ -11,3 +11,60 @@ if (isset($_SESSION['id'])) { //ログインしているとき
 ?>
 <h1><?php echo $msg; ?></h1>
 <?php echo $link; ?>
+
+
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="utf-8">
+  <title>blog一覧</title>
+</head>
+
+<body>
+  <?php
+  $dsn = "mysql:host=localhost; dbname=blog; charset=utf8mb4";
+  $db_account_name = "blog";
+  $db_account_password = "blog";
+  try {
+    $pdo = new PDO($dsn, $db_account_name, $db_account_password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+  } catch (PDOException $Exception) {
+    die('接続エラー：' . $Exception->getMessage());
+  }
+  try {
+    $sql = "SELECT * FROM blog.blogs";
+    $stmh = $pdo->prepare($sql);
+    $stmh->execute();
+  } catch (PDOException $Exception) {
+    die('接続エラー：' . $Exception->getMessage());
+  }
+  ?>
+  <h1>blog一覧</h1>
+  <table>
+    <tbody>
+      <tr>
+        <th>id</th>
+        <th>user_id</th>
+        <th>blog_title</th>
+      </tr>
+      <?php
+      while ($row = $stmh->fetch(PDO::FETCH_ASSOC)) {
+
+      ?>
+        <tr>
+          <th><?= htmlspecialchars($row['id']) ?></th>
+          <th><?= htmlspecialchars($row['user_id']) ?></th>
+          <th><?= htmlspecialchars($row['title']) ?></th>
+        </tr>
+      <?php
+
+      }
+      $pdo = null;
+      ?>
+    </tbody>
+  </table>
+</body>
+
+</html>
