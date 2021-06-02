07d843739dd0f9a51f043847cb39f72db79ca63b
tanaka katsuya <gu0520kt@gmail.com>
2021-06-03 08:02:44 +0900

    新規作成フォームの文言修正

--- create.php
+++ create.php
@@ -6,12 +6,12 @@ $dsn = "mysql:host=localhost; dbname=blog; charset=utf8mb4";
 $db_account_name = "blog";
 $db_account_password = "blog";
 try {
-    $pdo = new PDO($dsn, $db_account_name, $db_account_password, array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8mb4'));
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
-    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+  $pdo = new PDO($dsn, $db_account_name, $db_account_password, array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8mb4'));
+  $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+  $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 } catch (PDOException $e) {
-    exit('接続できませんでした。理由：' . $e->getMessage());
+  exit('接続できませんでした。理由：' . $e->getMessage());
 }
 // Postされたものを定義
 $user_id = $_SESSION['id'];
@@ -31,19 +31,19 @@ VALUES(?,?,?)
 
 $params = array($user_id, $blog_title, $content);
 try {
-    $statement = $pdo->prepare($sql);
-    $statement->execute($params);
-    $judgment = true;
+  $statement = $pdo->prepare($sql);
+  $statement->execute($params);
+  $judgment = true;
 } catch (PDOException $e) {
-    exit('接続できませんでした。理由：' . $e->getMessage());
+  exit('接続できませんでした。理由：' . $e->getMessage());
 }
 
 if ($judgment) {
-    $msg = '商品を登録しました。';
-    $link = '<a href="index.php">戻る</a>';
+  $msg = 'ブログ記事を登録しました。';
+  $link = '<a href="index.php">戻る</a>';
 } else {
-    $msg = '商品の登録に失敗しました。';
-    $link = '<a href="create_form.php">戻る</a>';
+  $msg = 'ブログ記事の登録に失敗しました。';
+  $link = '<a href="create_form.php">戻る</a>';
 }
 ?>
 <h1><?php echo $msg; ?></h1>
