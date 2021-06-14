51376330530af4acba43c8fa2d9e9b6630106fc2
tanaka katsuya <gu0520kt@gmail.com>
2021-06-14 09:55:04 +0900

    マイページで自分の記事を表示、一覧ページで他のユーザーの記事含めてすべての記事を表示するよう修正

--- myarticledetail.php
+++ myarticledetail.php
@@ -47,6 +47,14 @@ $res = $pdo->query($sql)->fetch(PDO::FETCH_ASSOC);
                       </div>
                     </div>
                     <div class="text-right mt-6">
+                      <a href="/edit.php?a=<?= htmlspecialchars($_GET["a"]) ?>">
+                        <button class="bg-yellow-300 text-black mx-auto active:bg-yellow-400 text-sm font-bold uppercase px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1" type="submit" style="transition: all 0.15s ease 0s;">編集
+                        </button>
+                      </a>
+                      <a href="/destroy.php?a=<?= htmlspecialchars($_GET["a"]) ?>">
+                        <button class="bg-yellow-300 text-black mx-auto active:bg-yellow-400 text-sm font-bold uppercase px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1" type="submit" style="transition: all 0.15s ease 0s;">削除
+                        </button>
+                      </a>
                       <a href="/index.php">
                         <button class="bg-yellow-300 text-black mx-auto active:bg-yellow-400 text-sm font-bold uppercase px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1" type="submit" style="transition: all 0.15s ease 0s;">一覧ページへ
                         </button>
--- mypage.php
+++ mypage.php
@@ -14,7 +14,7 @@ if (isset($_SESSION['id'])) { //ログインしているとき
 <head>
   <meta charset="utf-8">
   <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
-  <title>blog一覧</title>
+  <title>マイページ</title>
 </head>
 
 <div class="w-full">
@@ -34,7 +34,7 @@ if (isset($_SESSION['id'])) { //ログインしているとき
         </div>
       </div>
       <div class="flex flex-col md:flex-row hidden md:block -mx-2">
-        <a href="/mypage.php" class="text-gray-800 rounded hover:bg-gray-900 hover:text-gray-100 hover:font-medium py-2 px-2 md:mx-2">マイページ</a>
+        <a href="/index.php" class="text-gray-800 rounded hover:bg-gray-900 hover:text-gray-100 hover:font-medium py-2 px-2 md:mx-2">一覧ページ</a>
         <a href="/logout.php" class="text-gray-800 rounded hover:bg-gray-900 hover:text-gray-100 hover:font-medium py-2 px-2 md:mx-2">ログアウト</a>
       </div>
     </div>
@@ -64,27 +64,37 @@ if (isset($_SESSION['id'])) { //ログインしているとき
 
   <div class="blogs__wraper bg-green-300  py-20 px-20">
     <div class="ml-8 mb-12">
-      <h2 class="mb-2 px-2 text-6xl font-bold text-green-800">blog一覧</h2>
+      <h2 class="mb-2 px-2 text-6xl font-bold text-green-800">マイページ</h2>
+    </div>
+    <div class="mx-8 my-0">
+      <a href="/create.php">
+        <button class="bg-transparent hover:bg-green-800 text-gray-600 font-semibold hover:text-white py-2 px-4 border border-green-800 hover:border-transparent rounded">
+          新規作成
+        </button>
+      </a>
     </div>
     <div class="flex flex-wrap">
+
       <?php
       while ($row = $stmh->fetch(PDO::FETCH_ASSOC)) {
-        $limit = 15;
-        $content = mb_strimwidth(strip_tags($row['content']), 0, 15, '…', 'UTF-8');
+        if ($_SESSION['id'] == $row['user_id']) {
+          $limit = 15;
+          $content = mb_strimwidth(strip_tags($row['content']), 0, 15, '…', 'UTF-8');
       ?>
 
-        <div class="blogs bg-white w-1/5 m-8">
-          <div class="">
-            <img src="https://images.unsplash.com/photo-1489396160836-2c99c977e970?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60" class="">
+          <div class="blogs bg-white w-1/5 m-8">
+            <div class="">
+              <img src="https://images.unsplash.com/photo-1489396160836-2c99c977e970?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60" class="">
+            </div>
+            <div class="p-5">
+              <h1 class="text-2xl font-bold text-green-800 py-2"><?= htmlspecialchars($row['title']) ?></h1>
+              <p class="bg-white text-sm text-black"><?= htmlspecialchars($content) ?></p>
+              <a href="/myarticledetail.php/?a=<?= htmlspecialchars($row['id']) ?>" class="py-2 px-3 mt-4 px-6 text-white bg-green-500 inline-block rounded">記事詳細へ</a>
+            </div>
           </div>
-          <div class="p-5">
-            <h1 class="text-2xl font-bold text-green-800 py-2"><?= htmlspecialchars($row['title']) ?></h1>
-            <p class="bg-white text-sm text-black"><?= htmlspecialchars($content) ?></p>
-            <a href="/detail.php/?a=<?= htmlspecialchars($row['id']) ?>" class="py-2 px-3 mt-4 px-6 text-white bg-green-500 inline-block rounded">記事詳細へ</a>
-          </div>
-        </div>
 
       <?php
+        }
       }
       $pdo = null;
       ?>
