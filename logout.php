5705278c627101286bf0443f688a7b82228f1886
aoi0038 <aoi0038@gmail.com>
2021-07-30 11:40:48 +0900

    ディレクトリ構造の変更に伴い削除

--- logout.php
+++
@@ -1,8 +0,0 @@
-<?php 
-    session_start();
-
-    $_SESSION = [];
-    if (isset($_COOKIE[session_name()])) setcookie(session_name(), '', time()-4200, '/');
-    session_destroy();
-    header("Location: ./signin.php");
-    exit;
